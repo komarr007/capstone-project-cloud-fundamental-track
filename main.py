fc4e730d33ec9d21aba1e6e738e2f8f9b218615e
komarr007 <62511453+komarr007@users.noreply.github.com>
2021-11-19 13:26:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from azure.storage.blob import BlobServiceClient, __version__
 from azure.core.exceptions import ResourceNotFoundError
 import json
 
-connect_str = 'BlobEndpoint=https://clinicdumpmemory.blob.core.windows.net/;QueueEndpoint=https://clinicdumpmemory.queue.core.windows.net/;FileEndpoint=https://clinicdumpmemory.file.core.windows.net/;TableEndpoint=https://clinicdumpmemory.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-11T12:28:40Z&st=2021-11-14T04:28:40Z&spr=https&sig=aewhtO2kZiyAsNe7YH0AzhTk73K4FKmNH1v0iOxMEvY%3D'
-container_name = 'scan-lab'
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_client = blob_service_client.get_container_client(container_name)
-
 app = FastAPI()
 
 @app.get("/metadata")
-def get_meta():
+def get_meta(container_name):
     try:
+            connect_str = 'BlobEndpoint=https://clinicdumpmemory.blob.core.windows.net/;QueueEndpoint=https://clinicdumpmemory.queue.core.windows.net/;FileEndpoint=https://clinicdumpmemory.file.core.windows.net/;TableEndpoint=https://clinicdumpmemory.table.core.windows.net/;SharedAccessSignature=sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupitfx&se=2021-12-11T12:28:40Z&st=2021-11-14T04:28:40Z&spr=https&sig=aewhtO2kZiyAsNe7YH0AzhTk73K4FKmNH1v0iOxMEvY%3D'
+            blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+            container_client = blob_service_client.get_container_client(container_name)
             blobTampungan = []
             metaTampungan = []
             tampunganjadi = []
